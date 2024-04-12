aa4dfffbf5a79269c7308eb968c43b1c0abb54a9
Nagesh Tejwani <nagesh.tejwani@gmail.com>
2024-04-13 02:31:22 +0530

    Slack integration with webhook

--- src/main/java/com/sample/notification/service/channels/Slack.java
+++ src/main/java/com/sample/notification/service/channels/Slack.java
@@ -1,16 +1,41 @@
 package com.sample.notification.service.channels;
 
 import com.sample.notification.service.dto.Message;
-import com.sample.notification.service.dto.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component("slack")
-public class Slack implements Channel{
-    @Override
-    public void send(Message message) {
-        System.out.println("Slack sent: " + message.getBody());
+public class Slack implements Channel {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Slack.class);
+
+
+    private final WebClient webClient;
+
+
+    public Slack(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://hooks.slack.com/services/XXXXX/XXXXX/XXXXX").build();// Needs to fix with the yaml value
     }
 
+    @Override
+    public void send(Message message) {
+        String json = "{" +
+                "\"text\":\"" +
+                message.getBody() +
+                "\"" +
+                "}";
 
+        webClient.post()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(json))
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnSuccess(response -> LOGGER.info("Message sent to Slack: " + message.getBody()))
+                .doOnError(e -> LOGGER.error("Failed to send message to Slack: ", e))
+                .subscribe();
+    }
 }
